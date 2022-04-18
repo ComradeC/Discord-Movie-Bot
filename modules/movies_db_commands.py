e9f85933c0ab6aaa34cad10ce0f050555acdfead
zin-deimon <zindeimon@gmail.com>
2022-04-18 15:35:25 +0300

    changed DB settings to match server

--- modules/movies_db_commands.py
+++ modules/movies_db_commands.py
@@ -1,8 +1,8 @@
 from discord.ext import commands
 import psycopg2
 
-conn = psycopg2.connect(dbname='moviebotdb', user='postgres', password='admin')
-
+#conn = psycopg2.connect(dbname='moviebotdb', user='postgres', password='admin')    #local test connection
+conn = psycopg2.connect(dbname='postgres', user='root', password='root')            #public server connection
 
 class MoviesDB(commands.Cog):
 
