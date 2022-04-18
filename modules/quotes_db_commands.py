efcc5e4c6ec6eb3ef901f60b2b6d9dc3a7de7a0c
zin-deimon <zindeimon@gmail.com>
2022-04-18 15:36:11 +0300

    changed DB settings to match server

--- modules/quotes_db_commands.py
+++ modules/quotes_db_commands.py
@@ -5,8 +5,8 @@ import time
 from discord.ext import commands
 import psycopg2
 
-conn = psycopg2.connect(dbname='moviebotdb', user='postgres', password='admin')
-
+#conn = psycopg2.connect(dbname='moviebotdb', user='postgres', password='admin')    #local test connection
+conn = psycopg2.connect(dbname='postgres', user='root', password='root')            #public server connection
 
 class QuotesDB(commands.Cog):
 
