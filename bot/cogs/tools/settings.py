119f447b9aa9d988222bb3be01650b98b2ec28d1
ComradeC <commando1537@gmail.com>
2022-07-07 12:07:24 +0300

    website split

--- bot/cogs/tools/settings.py
+++ bot/cogs/tools/settings.py
@@ -2,11 +2,9 @@
 import os
 
 # external modules
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 engine = create_engine(os.environ['SQL_CONNECTION'], echo=False, future=True)
 
