efd4bba0c41ef9137793f028c4f6c3a23b6cb9d4
ComradeC <commando1537@gmail.com>
2023-07-22 16:55:53 +0300

    fallback IMDB id scraping

--- bot/cogs/tools/settings.py
+++ bot/cogs/tools/settings.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+KP_TOKEN = os.environ['KP_TOKEN']
 engine = create_engine(os.environ['SQL_CONNECTION'], echo=True, future=True)
 
 Session = sessionmaker(engine)
